9750e0c0277c25e41b68cab795cdf57bce8015be
Seth Orell <seth.orell@gmail.com>
2023-07-31 11:04:55 -0800

    Invoke service

--- jobs/handlers/onJobCreated.js
+++ jobs/handlers/onJobCreated.js
@@ -2,11 +2,13 @@ import middy from '@middy/core';
 import inputOutputLogger from '@middy/input-output-logger';
 
 import ioLoggerConfig from '../../common/ioLoggerConfig';
+import * as service from '../service';
 
 const onJobCreated = async (event) => {
-  const ph = { ...event };
-
-  return ph;
+  const {
+    detail: { jobId, phrase, name },
+  } = event;
+  await service.startStateMachine({ jobId, phrase, name });
 };
 
 export default middy(onJobCreated).use(inputOutputLogger(ioLoggerConfig));
